2b46a96ef384bf1a6258c56f0fa134fe907d7f51
Igor Storozhuk <igorstorozhuk@MacBook-Pro-Igor.local>
2023-06-15 18:13:13 +0300

    add put delete

--- controllers/todos.js
+++ controllers/todos.js
@@ -14,7 +14,6 @@ const TodosController = {
   },
   post: async(req, res) => {
     try {
-      console.log(req.body)
       const {description} = req.body;  
       const todo = await pool.query('INSERT INTO Todos (description) VALUES ($1) RETURNING *', [description]);
       res.json(todo.rows[0]);
@@ -23,6 +22,27 @@ const TodosController = {
       res.status(500).send('Internal Server Error');
     }
   },
+  put: async(req, res) => {
+    try {
+      const {id} = req.params;
+      const {description} = req.body;  
+      const todo = await pool.query('UPDATE Todos SET description = $1 WHERE id = $2 RETURNING *', [description, id]);
+      res.json(todo.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+  delete: async(req, res) => {  
+    try {
+      const {id} = req.params; 
+      await pool.query('DELETE FROM Todos WHERE id = $1', [id]);
+      res.json(`todo with id ${id} was deleted`);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  }
 };
   
 module.exports = TodosController;
--- routers/server.js
+++ routers/server.js
@@ -7,4 +7,8 @@ serverRouter.get('/todos', TodosController.get);
 
 serverRouter.post('/todos', TodosController.post);
 
+serverRouter.put('/todos/:id', TodosController.put);
+
+serverRouter.delete('/todos/:id', TodosController.delete);
+
 module.exports = {serverRouter};
